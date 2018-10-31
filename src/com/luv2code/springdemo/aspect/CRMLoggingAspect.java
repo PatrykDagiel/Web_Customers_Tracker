98e7e5369ef48a7b55e8fdfe62000ba8474dedef
Dagiel, Patryk (Nokia - PL/Wroclaw) <patryk.dagiel@nokia.com>
2018-10-31 16:16:33 +0100

    Past 383.

--- src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
+++ src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.luv2code.springdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
@@ -11,6 +12,25 @@ public class CRMLoggingAspect {
     // setup logger
     private Logger myLogger = Logger.getLogger(getClass().getName());
 
+    // setup pointcut declarations
+    @Pointcut("execution(* com.luv2code.springdemo.controller.*(..))")
+    private void forControllerPackage() {}
+
+    // setup pointcut declarations
+    @Pointcut("execution(* com.luv2code.springdemo.service.*(..))")
+    private void forServicePackage() {}
+
+    // setup pointcut declarations
+    @Pointcut("execution(* com.luv2code.springdemo.dao.*(..))")
+    private void forDaoPackage() {}
+
+    @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+    private void forAppFlow() {}
+
+
+    // add @Before advice
+
+    // add @AfterReturning advice
 
 
 }
